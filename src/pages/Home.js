624092aab5303f749338d6ca1416f5eba5217c6e
007steve <stephen.plummer1.sp@gmail.com>
2021-02-24 15:09:09 -0500

    added fetch to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import Nav from '../components/Nav'
-
+import React, { useEffect, useState } from "react";
+import Sidebar from "../components/Sidebar";
+import Feed from "../components/Feed";
+import "../styles/Home.css";
 function Home() {
-    return (
-        <div>
-            <Nav/>
-        </div>
-    )
+  //fetch
+  useEffect(() => {
+    fetch("http://localhost:8000/api/users/loggedIn", {
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+  return (
+    <div className="home">
+      <Sidebar />
+      <Feed />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
