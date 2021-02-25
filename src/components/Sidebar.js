d89393a9a9a17395d630e88ff59b8b7d5197b361
007steve <stephen.plummer1.sp@gmail.com>
2021-02-25 00:52:35 -0500

    added logout

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "../styles/Sidebar.css";
-import StarOutlineIcon from "@material-ui/icons/StarOutline";
 import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
 import CreateIcon from "@material-ui/icons/Create";
-import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
+import ExitToAppOutlinedIcon from "@material-ui/icons/ExitToAppOutlined";
+import { Link, useHistory } from "react-router-dom";
 function Sidebar() {
+  let history = useHistory();
+  const handlelogout = () => {
+    fetch("http://localhost:8000/api/users/logout")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+    history.push("/login");
+  };
   return (
     <div className="sidebar">
       <div className="sidebar__logoContainer">
@@ -15,7 +22,6 @@ function Sidebar() {
           src="https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo.png"
           alt=""
         />
-        
       </div>
       <div className="sidebar__container">
         <div className="sidebar__linkContainer">
@@ -34,12 +40,11 @@ function Sidebar() {
           <CreateIcon />
           <p className="sidebar__title">Post</p>
         </div>
-        <div className="sidebar__linkContainer">
-          <ExitToAppOutlinedIcon  />
+        <div className="sidebar__linkContainer" onClick={handlelogout}>
+          <ExitToAppOutlinedIcon />
           <p className="sidebar__title">Logout</p>
         </div>
       </div>
-     
     </div>
   );
 }
